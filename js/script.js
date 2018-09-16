6c75d52813e123339f91ff3d1faeefb2f7d23388
NZKea <bookwormnathan@gmail.com>
2018-09-17 09:54:50 +1200

    Macbook

--- js/script.js
+++ js/script.js
@@ -6,7 +6,22 @@ document.addEventListener('DOMContentLoaded', function(){
     new Cocoen();
   };
   if (page==="shop.html") {
-      ImageMap('img[usemap]')
+
+
+    var coll = document.getElementsByClassName("collapsible");
+    var i;
+
+    for (i = 0; i < coll.length; i++) {
+      coll[i].addEventListener("click", function() {
+        this.classList.toggle("cactive");
+        var content = this.nextElementSibling;
+        if (content.style.display === "block") {
+          content.style.display = "none";
+        } else {
+          content.style.display = "block";
+        }
+      });
+} 
       
     }
 });
